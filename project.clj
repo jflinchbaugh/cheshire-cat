ac83144f1e7a2234e2138531d4c5675c0d314ec0
John Flinchbaugh <john@hjsoft.com>
2018-03-07 21:19:10 -0500

    add clojurescript support

--- project.clj
+++ project.clj
@@ -7,9 +7,27 @@
     [compojure "1.5.1"]
     [ring/ring-defaults "0.2.1"]
     [ring/ring-json "0.4.0"]
+    [org.clojure/clojurescript "1.9.946"]
   ]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [
+    [lein-ring "0.9.7"]
+    [lein-cljsbuild "1.1.7"]
+  ]
   :ring {:handler cheshire-cat.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  :profiles {
+    :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}
+  }
+  :cljsbuild {
+    :builds [
+      {
+        :source-paths ["src-cljs"]
+        :compiler {
+          :output-to "war/javascripts/main.js"
+          :optimizations :whitespace
+          :pretty-print true
+        }
+      }
+    ]
+  }
+)
